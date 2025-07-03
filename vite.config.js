b63d22f1264ab5bcfd027d040758d97a00e04199
Navaneetha-chandrasekaran <navaneethachandrasekaran3@gmail.com>
2025-07-03 22:20:42 +0530

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'Timer_challenge'
+  base:'/TimerChallenge/'
 })
 
